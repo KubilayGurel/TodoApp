6e08ab339c7946dc51795a5acfe8f628f880ede3
Kubilay Gürel <kubilay.kubi99@gmail.com>
2025-02-10 15:33:34 +0300

    feat: Implement notification worker and add notification permission
    This commit introduces a notification worker to handle scheduled notifications and adds the necessary permission to the `AndroidManifest.xml`. Key changes include:
    
    -   Added `NotificationWorker.kt` to handle the creation and display of notifications.
    -   Implemented logic to create a notification channel for the Todo app, setting the channel ID, name, and importance.
    - Implemented doWork logic to build and show the notification.
    -   Added `POST_NOTIFICATIONS` permission to `AndroidManifest.xml` to enable the app to post notifications.

---
+++ app/src/main/java/com/kubilaygurel/todoapp/data/util/NotificationWorker.kt
@@ -0,0 +1,54 @@
+package com.kubilaygurel.todoapp.data.util
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.kubilaygurel.todoapp.R
+
+class NotificationWorker(
+    private val context: Context,
+    workerParams: WorkerParameters
+) : Worker(context, workerParams) {
+
+    companion object {
+        const val CHANNEL_ID = "todo_channel"
+        const val CHANNEL_NAME = "Todo Notifications"
+    }
+
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                description = "Todo app notifications"
+            }
+
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    override fun doWork(): Result {
+        createNotificationChannel()
+        val title = inputData.getString("title") ?: "Notification"
+        val content = inputData.getString("content") ?: "There is a todo!"
+        
+        val notification = NotificationCompat.Builder(applicationContext,CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(title)
+            .setContentText(content)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+
+        NotificationManagerCompat.from(applicationContext).notify(1, notification)
+        return Result.success()
+    }
+}
